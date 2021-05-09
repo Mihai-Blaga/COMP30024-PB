f009be8152d52878bb2bb37e4edb0588972316f6
225544 <murraypeh@gmail.com>
2021-05-09 21:29:36 +1000

    Evaluate update

--- m/action.py
+++ m/action.py
@@ -44,11 +44,12 @@ def evaluate(state, player):
     Given a board state and the player making a move, evaluates the current state of the board
     """
     #return temp_evaluate(state, player)
+    
     #setup
     opponent = m.util.calculate_opponent(player)
 
     throws_left = state[player + "_throws"]
-    opponent_throws_left = state[opponent + "_throws"]
+    enemy_throws_left = state[opponent + "_throws"]
 
     #setting throw boundaries
     player_upper = 4
@@ -56,17 +57,17 @@ def evaluate(state, player):
     opponent_upper = 4
     opponent_lower = -4
 
-    if (throws_left > 0):
-        if (player == "lower"):
-            player_upper = (player_upper - throws_left) + 1
-        if (player == "upper"):
-            player_lower = (player_lower + throws_left) - 1
+    #if (throws_left > 0):
+    #    if (player == "lower"):
+    #        player_upper = (player_upper - throws_left) + 1
+    #    if (player == "upper"):
+    #        player_lower = (player_lower + throws_left) - 1
 
-    if (opponent_throws_left > 0):
-        if (opponent == "lower"):
-            opponent_upper = (opponent_upper - opponent_throws_left) + 1
-        if (opponent == "upper"):
-            opponent_lower = (opponent_lower + opponent_throws_left) - 1
+    #if (enemy_throws_left > 0):
+    #    if (opponent == "lower"):
+    #        opponent_upper = (opponent_upper - enemy_throws_left) + 1
+    #    if (opponent == "upper"):
+    #        opponent_lower = (opponent_lower + enemy_throws_left) - 1
         
 
     #dictionary of dictionaries based on piece label
@@ -96,17 +97,20 @@ def evaluate(state, player):
     #Yep
     closest_of_all_attackers = 10
     closest_of_all_targets = 10
+    closest_target = 10
+    closest_attacker = 10
 
     for piece in state[player]:
         closest_target = 10
         closest_attacker = 10
         
         attacker = no_to_piece[(piece_to_no[piece[0]] + 1) % 3]
-        target = no_to_piece[(piece_to_no[piece[0]] - 1) % 3]
+        target = no_to_piece[(piece_to_no[piece[0]] + 2) % 3]
 
         if (piece[1] > opponent_lower and piece[1] < opponent_upper):
             #piece is within throw range
-            closest_attacker = 1
+            pass
+            #closest_attacker = 1
         else:
             for a in pieces[opponent][attacker]:
                 dist = m.util.calc_dist(a[0],a[1],piece[1],piece[2])
@@ -120,7 +124,7 @@ def evaluate(state, player):
             player_num_vulnerable_pieces = player_num_vulnerable_pieces + 1
         elif closest_attacker == 10:
             player_temp_safe[piece_to_no[piece[0]]] = 1
-            if opponent_throws_left == 0:
+            if enemy_throws_left == 0:
                 player_piece_invul[piece_to_no[piece[0]]] = 1
 
         sum_to_attackers = sum_to_attackers + closest_attacker
@@ -129,48 +133,53 @@ def evaluate(state, player):
         closest_of_all_attackers = min(closest_of_all_attackers, closest_attacker)
         closest_of_all_targets = min(closest_of_all_targets, closest_target)
 
+    if closest_of_all_attackers == 10:
+        closest_of_all_attackers = 0
+    #if closest_of_all_targets == 10:
+    #    closest_of_all_targets = 10
     
     for piece in state[opponent]:
         closest_attacker = 10
-        closest_target = 10
-
+    #    closest_target = 10
+    #
         attacker = no_to_piece[(piece_to_no[piece[0]] + 1) % 3]
-        target = no_to_piece[(piece_to_no[piece[0]] - 1) % 3]
-
-        if (piece[1] < player_upper and piece[1] > player_lower):
-            closest_target = 1
-        else:
-            for a in pieces[opponent][attacker]:
-                dist = m.util.calc_dist(a[0],a[1],piece[1],piece[2])
-                closest_attacker = min(closest_attacker, dist)
-        
-        if closest_attacker == 1:
-            enemy_num_vulnerable_pieces = enemy_num_vulnerable_pieces + 1
-        elif closest_attacker == 10:
+    #    target = no_to_piece[(piece_to_no[piece[0]] - 1) % 3]
+    #
+    #    if (piece[1] < player_upper and piece[1] > player_lower):
+    #        closest_target = 1
+    #    else:
+        for a in pieces[player][attacker]:
+            dist = m.util.calc_dist(a[0],a[1],piece[1],piece[2])
+            closest_attacker = min(closest_attacker, dist)
+    #    
+    #    if closest_attacker == 1:
+    #        enemy_num_vulnerable_pieces = enemy_num_vulnerable_pieces + 1
+        if closest_attacker == 10:
             enemy_temp_safe[piece_to_no[piece[0]]] = 1
-            if throws_left == 0:
-                enemy_piece_invul[piece_to_no[piece[0]]] = 1
+     #       if throws_left == 0:
+    #            enemy_piece_invul[piece_to_no[piece[0]]] = 1
 
     #sum_to_targets = sum_to_targets + closest_target
-    closest_of_all_targets = min(closest_of_all_targets, closest_target)
+    
 
-    #scoring function
-    score = sum_to_attackers*attacker_proximity
-    score = score + closest_of_all_attackers*shyness
-    score = score + sum_to_targets*target_proximity
-    score = score + closest_of_all_targets*aggression
-    score = score + p_p_l*player_pieces_left
-    score = score + e_p_l * enemy_pieces_left
-    score = score + p_n_v_p * player_num_vulnerable_pieces
-    score = score + e_n_v_p * enemy_num_vulnerable_pieces
-    score = score + p_p_i * sum(player_piece_invul)
-    score = score + e_p_i * sum(enemy_piece_invul)
-    score = score + p_t_s * sum(player_temp_safe)
-    score = score + e_t_s * sum(enemy_temp_safe)
-    score = score + t_c * throws_left
+    if enemy_pieces_left == 1 and sum(player_piece_invul) > 0:
+        if sum(enemy_piece_invul) == 0:
+            score = 1000
+        else:
+            score = 50
+    elif sum(enemy_piece_invul) > 0:
+        score = -1000
+    else:
+        score = 50 + 20*(player_pieces_left + throws_left - enemy_pieces_left  - enemy_throws_left) + 2*(9 - closest_of_all_targets) + 15*throws_left -5*(sum(enemy_temp_safe))
+        if closest_of_all_attackers == 1:
+            score = score - 5
+        #score = 50 + 20*(player_pieces_left + throws_left - enemy_pieces_left  - enemy_throws_left) + 2*(9 - closest_of_all_targets) + 15*throws_left -5*(sum(enemy_temp_safe))
 
+        
+    #print(state)
+    #print(score)
     log(state)
-    log(sum_to_attackers*attacker_proximity,closest_of_all_attackers*shyness,sum_to_targets*target_proximity,closest_of_all_targets*aggression,p_p_l*player_pieces_left,e_p_l * enemy_pieces_left,p_n_v_p * player_num_vulnerable_pieces,e_n_v_p * enemy_num_vulnerable_pieces,p_p_i * sum(player_piece_invul),e_p_i * sum(enemy_piece_invul),p_t_s * sum(player_temp_safe),e_t_s * sum(enemy_temp_safe),t_c * throws_left)
+    #log(sum_to_attackers*attacker_proximity,closest_of_all_attackers*shyness,sum_to_targets*target_proximity,closest_of_all_targets*aggression,p_p_l*player_pieces_left,e_p_l * enemy_pieces_left,p_n_v_p * player_num_vulnerable_pieces,e_n_v_p * enemy_num_vulnerable_pieces,p_p_i * sum(player_piece_invul),e_p_i * sum(enemy_piece_invul),p_t_s * sum(player_temp_safe),e_t_s * sum(enemy_temp_safe),t_c * throws_left)
     log(score)
 
     return score
@@ -218,20 +227,37 @@ def make_best_move(moves, state, player, max = True):
 def convert(state, player, hex, key):
     #returns the move in proper format
     if (key < 0):
-        pieces = state[player]
-        counts = [0,0,0]
+        opponent = m.util.calculate_opponent(player)
+        counts_player = [0,0,0]
+        counts_enemy = [0,0,0]
         #count all pieces this player has
-        for p in pieces:
-            counts[piece_to_no[p[0]]] = counts[piece_to_no[p[0]]] + 1
+        for p in state[player]:
+            counts_player[piece_to_no[p[0]]] = counts_player[piece_to_no[p[0]]] + 1
+        
+        for p in state[opponent]:
+            counts_enemy[piece_to_no[p[0]]] = counts_enemy[piece_to_no[p[0]]] + 1
 
         #randomly choose counter with fewest pieces
         best = []
-        if (counts[0] <= counts[1] and counts[0] <= counts[2]):
-            best.append(0)
-        if (counts[1] <= counts[0] and counts[1] <= counts[2]):
-            best.append(1)
-        if (counts[2] <= counts[1] and counts[2] <= counts[0]):
-            best.append(2) 
+        diff = 10
+        
+        i = 0
+        while i < 3:
+            temp_diff = counts_player[i] - counts_enemy[(i+2)%3]
+            if temp_diff == diff:
+                best.append(i)
+            elif temp_diff < diff:
+                diff = temp_diff
+                best = [i]
+            i = i + 1
+        
+
+        #if (counts_player[0] <= counts[1] and counts[0] <= counts[2]):
+        #    best.append(0)
+        #if (counts[1] <= counts[0] and counts[1] <= counts[2]):
+        #    best.append(1)
+        #if (counts[2] <= counts[1] and counts[2] <= counts[0]):
+        #    best.append(2) 
 
         return output_move(key, no_to_piece[random.choice(best)], hex)
     else:
